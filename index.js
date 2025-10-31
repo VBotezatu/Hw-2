900cb5c495be4abd844e17e5816a46256fa7d547
VBotezatu <148051501+VBotezatu@users.noreply.github.com>
2025-10-31 10:12:17 +0100

    ready

---
+++ index.js
@@ -0,0 +1,47 @@
+let celsius = 25;
+let fahrenheit = (celsius * 9 / 5 + 32); 
+console.log("Температура у Фаренгейтах:", fahrenheit);
+
+
+let daysInMonth = 30;
+let hoursInMonth = daysInMonth * 24; 
+let minutesInMonth = hoursInMonth * 60; 
+console.log("Годин у місяці:", hoursInMonth);
+console.log("Хвилин у місяці:", minutesInMonth);
+
+
+let health = 100; 
+let energy = 80; 
+health = health - 20; 
+energy = energy - 30;
+console.log("Здоров’я після гри:", health);
+console.log("Енергія після грит:", energy);
+
+
+let totalPrice = 100;
+let discount = totalPrice * 0.9;
+let discountedPrice = discount;
+console.log("Ваша ціна після скидки є:", discountedPrice);
+
+
+const floatNumber = 12.78;
+let roundedDown = Math.floor(floatNumber);
+console.log(roundedDown);
+
+
+const floatString = 45.67;
+let parsedFloat = parseFloat(floatString);
+console.log(parsedFloat);
+
+
+const intString = "123";
+let parsedInt = parseInt(intString);
+console.log(parsedInt);
+
+
+let number = 49;
+let sqrtNumber = Math.sqrt(number);
+console.log(sqrtNumber);
+
+
+
